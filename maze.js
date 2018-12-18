eaffdf7ecf4402c03adbe1921c9e3a4000bedbb2
NissimHadar <nissim.hadar@gmail.com>
2018-12-18 09:27:42 -0800

    Added maze Delete entities on end.

--- maze.js
+++ maze.js
@@ -1,8 +1,7 @@
 var position = MyAvatar.position;
 
-entities = [];
 	
-entities.push(Entities.addEntity({
+var base = Entities.addEntity({
 	name : "Maze Base",
 	type: "Model",
 	color: { red: 100, green: 100, blue: 100 },
@@ -13,10 +12,25 @@ entities.push(Entities.addEntity({
 	visible: true,
 	dynamic: true,
 	"shapeType": "static-mesh",
-	userData: JSON.stringify({ grabbableKey: { grabbable: false } })
-}));
+	userData: JSON.stringify({ grabbableKey: { grabbable: true } })
+});
 	
-entities.push(Entities.addEntity({
+var maze = Entities.addEntity({
+	name : "Maze",
+	type: "Model",
+	color: { red: 100, green: 100, blue: 100 },
+	modelURL: 'file:///C:/maze/maze/maze.obj',
+	dimensions: { x: 0.45, y: 0.45, z: 0.1 },
+	position: Vec3.sum(position, {x: 0.0, y: 0.3, z: 0.0 }),   
+	rotation: Quat.fromPitchYawRollDegrees(-90.0, 0.0, 0.0),    
+	visible: true,
+	dynamic: true,
+	parentID: base.sessionUUID,
+	"shapeType": "static-mesh",
+	userData: JSON.stringify({ grabbableKey: { grabbable: false } })
+});
+	
+var ball = Entities.addEntity({
 	name: "Maze Ball",
 	type: "Sphere",
 	color: { red: 0, green: 255, blue: 255 },
@@ -25,4 +39,11 @@ entities.push(Entities.addEntity({
 	gravity: {x: 0.0, y: -1.0, z: 0.0 },   
 	visible: true,
 	dynamic: true
-}));
+});
+
+// Delete all entities when finished
+Script.scriptEnding.connect(function () {
+  Entities.deleteEntity(base);
+  Entities.deleteEntity(maze);
+  Entities.deleteEntity(ball);
+});

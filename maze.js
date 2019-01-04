0b991a0aba02f21fc32e17fd75a4fe0e27c01133
NissimHadar <nissim.hadar@gmail.com>
2019-01-04 15:01:03 -0800

    Added ball collector

--- maze.js
+++ maze.js
@@ -20,7 +20,7 @@ var maze = Entities.addEntity({
 	type: "Model",
 	color: { red: 100, green: 100, blue: 100 },
 	modelURL: 'file:///C:/maze/maze/maze.obj',
-	dimensions: { x: 0.45, y: 0.45, z: 0.1 },
+	dimensions: { x: 0.46, y: 0.46, z: 0.1 },
 	localPosition: {x: 0.0, y: 0.0, z: 0.05 },   
 	localRotation: Quat.fromPitchYawRollDegrees(0.0, 0.0, 0.0),    
 	visible: true,
@@ -42,10 +42,27 @@ var ball = Entities.addEntity({
     kinematic: false,
 	dynamic: true
 });
+	
+var ballCollector = Entities.addEntity({
+	name : "Ball Collector",
+	type: "Model",
+	color: { red: 100, green: 100, blue: 100 },
+	modelURL: 'file:///C:/maze/ballCollector/ballCollector.obj',
+	dimensions: { x: 0.050, y: 0.062, z: 0.025 },
+	localPosition: {x: -0.28, y: -0.22, z: -0.089 },   
+	localRotation: Quat.fromPitchYawRollDegrees(0.0, 0.0, 270.0),    
+	visible: true,
+	dynamic: true,
+    kinematic: false,
+	parentID: base,
+	"shapeType": "static-mesh",
+	userData: JSON.stringify({ grabbableKey: { grabbable: false } })
+});
 
 // Delete all entities when finished
 Script.scriptEnding.connect(function () {
   Entities.deleteEntity(base);
   Entities.deleteEntity(maze);
   Entities.deleteEntity(ball);
+  Entities.deleteEntity(ballCollector);
 });
